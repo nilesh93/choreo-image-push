8da719e4324985cf73a9ae1c4949786e839f9fe7
achala dias <achalad@wso2.com>
2022-11-22 11:26:24 +0530

    Code improvement

--- index.js
+++ index.js
@@ -25,67 +25,75 @@ async function run() {
 }
 
 async function ecrLogin(cred) {
-  const username = cred.credentials.registryUser;
-  const password = cred.credentials.registryPassword;
-  const region = cred.credentials.region;
+  try {
+    const username = cred.credentials.registryUser;
+    const password = cred.credentials.registryPassword;
+    const region = cred.credentials.region;
 
-  var conifgChild = spawn(`aws configure set aws_access_key_id ${username} && aws configure set aws_secret_access_key ${password} && aws configure set default.region ${region} && aws ecr-public get-login-password --region ${region} | docker login --username AWS --password-stdin public.ecr.aws && aws ecr-public describe-repositories --repository-names ${choreoApp} || aws ecr-public create-repository --repository-name ${choreoApp}`,
-    {
-      shell: true
+    var conifgChild = spawn(`aws configure set aws_access_key_id ${username} && aws configure set aws_secret_access_key ${password} && aws configure set default.region ${region} && aws ecr-public get-login-password --region ${region} | docker login --username AWS --password-stdin public.ecr.aws && aws ecr-public describe-repositories --repository-names ${choreoApp} || aws ecr-public create-repository --repository-name ${choreoApp}`,
+      {
+        shell: true
+      });
+
+    conifgChild.stderr.on('data', function (data) {
+      core.setOutput("STDERR:", data.toString());
     });
-
-  conifgChild.stderr.on('data', function (data) {
-    core.setFailed("STDERR:", data.toString());
-  });
-  conifgChild.stdout.on("data", data => {
-    console.log(data.toString());
-  });
-  conifgChild.on('exit', async function (exitCode) {
-    core.setOutput("Config Child exited with code: " + exitCode);
-    if (exitCode != 0) {
-      process.exit(exitCode);
-    }
-    core.setOutput("Pushing ECR image with succeeded login");
-    await dockerPush(cred);
-  });
+    conifgChild.stdout.on("data", data => {
+      console.log(data.toString());
+    });
+    conifgChild.on('exit', async function (exitCode) {
+      core.setOutput("Config Child exited with code: " + exitCode);
+      if (exitCode != 0) {
+        process.exit(exitCode);
+      }
+      core.setOutput("Pushing ECR image with succeeded login");
+      await dockerPush(cred);
+    });
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
 
 async function acrLogin(cred) {
-  const username = cred.credentials.registryUser;
-  const password = cred.credentials.registryPassword;
-  const loginServer = cred.credentials.registry;
-  const authenticationToken = Buffer.from(`${username}:${password}`).toString('base64');
-  let config;
-  const runnerTempDirectory = process.env['RUNNER_TEMP']; // Using process.env until the core libs are updated
-  const dirPath = process.env['DOCKER_CONFIG'] || path.join(runnerTempDirectory, `docker_login_${Date.now()}`);
-  await io.mkdirP(dirPath);
-  const dockerConfigPath = path.join(dirPath, `config.json`);
-  if (fs.existsSync(dockerConfigPath)) {
-    try {
-      config = JSON.parse(fs.readFileSync(dockerConfigPath, 'utf8'));
-      if (!config.auths) {
-        config.auths = {};
-      }
-      config.auths[loginServer] = { auth: authenticationToken };
-    }
-    catch (err) {
-      // if the file is invalid, just overwrite it
-      config = undefined;
-    }
-  }
-  if (!config) {
-    config = {
-      "auths": {
-        [loginServer]: {
-          auth: authenticationToken
+  try {
+    const username = cred.credentials.registryUser;
+    const password = cred.credentials.registryPassword;
+    const loginServer = cred.credentials.registry;
+    const authenticationToken = Buffer.from(`${username}:${password}`).toString('base64');
+    let config;
+    const runnerTempDirectory = process.env['RUNNER_TEMP']; // Using process.env until the core libs are updated
+    const dirPath = process.env['DOCKER_CONFIG'] || path.join(runnerTempDirectory, `docker_login_${Date.now()}`);
+    await io.mkdirP(dirPath);
+    const dockerConfigPath = path.join(dirPath, `config.json`);
+    if (fs.existsSync(dockerConfigPath)) {
+      try {
+        config = JSON.parse(fs.readFileSync(dockerConfigPath, 'utf8'));
+        if (!config.auths) {
+          config.auths = {};
         }
+        config.auths[loginServer] = { auth: authenticationToken };
       }
-    };
+      catch (err) {
+        // if the file is invalid, just overwrite it
+        config = undefined;
+      }
+    }
+    if (!config) {
+      config = {
+        "auths": {
+          [loginServer]: {
+            auth: authenticationToken
+          }
+        }
+      };
+    }
+    core.debug(`Writing docker config contents to ${dockerConfigPath}`);
+    fs.writeFileSync(dockerConfigPath, JSON.stringify(config));
+    core.exportVariable('DOCKER_CONFIG', dirPath);
+    console.log('DOCKER_CONFIG environment variable is set');
+  } catch (error) {
+    core.setFailed(error);
   }
-  core.debug(`Writing docker config contents to ${dockerConfigPath}`);
-  fs.writeFileSync(dockerConfigPath, JSON.stringify(config));
-  core.exportVariable('DOCKER_CONFIG', dirPath);
-  console.log('DOCKER_CONFIG environment variable is set');
 }
 
 async function dockerPush(cred) {
@@ -97,7 +105,7 @@ async function dockerPush(cred) {
       shell: true
     });
     child.stderr.on('data', function (data) {
-      console.error("STDERR:", data.toString());
+      core.setOutput("STDERR:", data.toString());
     });
     child.stdout.on("data", data => {
       console.log(data.toString());
